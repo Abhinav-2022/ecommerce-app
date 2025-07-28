e727ac1a4dad6720154d2e1dfbb8b1e6352840e6
Abhinav-2022 <abhinavashokachu@gmail.com>
2025-07-28 20:14:27 +0530

    28-07-2025

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { Routes,Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Collection from "./Pages/Collection";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Product from "./Pages/Product";
+import Cart from './Pages/Cart';
+import Login from './Pages/Login';
+import Placeorder from './Pages/Placeorder';
+import Order from './Pages/Orders';
+import Navbar from "./components/Navbar";
 
 const App = () =>{
   return(
-    <div>
-
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"> 
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="collction" element={<Collection/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+      <Route path="/product/:productId" element={<Product/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path="/Login" element={<Login/>} />
+      <Route path="/place-order" element={<Placeorder/>}/>
+      <Route path="/Order" element={<Order/>}/>
+    </Routes>
+         
     </div>
   )
 
 }
 export default App
+
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex-item-center justify-between'>
+        
+
+    </div>
+  )
+}
+
+export default Navbar
