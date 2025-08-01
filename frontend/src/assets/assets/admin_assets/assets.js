024f64a1073b685d0d99fe6eccdc6b2a7014b78d
Abhinav-2022 <abhinavashokachu@gmail.com>
2025-08-01 19:55:12 +0530

    01-08-2025

--- frontend/src/assets/assets/admin_assets/assets.js
+++ frontend/src/assets/assets/admin_assets/assets.js
@@ -3,12 +3,17 @@ import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
+import search_icon from './search_icon'
+import profile_icon from './search_icon'
+
 
 export const assets = {
     logo,
     add_icon,
     order_icon,
     upload_area,
-    parcel_icon
+    parcel_icon,
+    search_icon,
+    profile_icon
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,59 +1,57 @@
 import React from 'react';
-import {assets} from '../assets/assets/admin_assets/assets';
-import {NavLink} from 'react-router-dom';
-
+import { assets } from '../assets/assets/frontend_assets/assets';
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-       <img src={assets.logo} className='w-36' alt="" />
-        <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-
+      <img src={assets.logo} className='w-36' alt="Logo" />
+      
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+        <li>
           <NavLink to='/' className='flex flex-col items-center gap-1'>
             <p>HOME</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden '/>
-              
-           </NavLink>
-            <NavLink to='/collection'className='flex flex-col items-center gap-1'>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/collection' className='flex flex-col items-center gap-1'>
             <p>COLLECTION</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700  hidden'/>
-              
-           </NavLink>
-          <NavLink to='/about'className='flex flex-col items-center gap-1'>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/about' className='flex flex-col items-center gap-1'>
             <p>ABOUT</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-              
-           </NavLink>
-            <NavLink to='/contact'className='flex flex-col items-center gap-1'>
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/contact' className='flex flex-col items-center gap-1'>
             <p>CONTACT</p>
-            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
-              
-           </NavLink> 
+            <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+          </NavLink>
+        </li>
+      </ul>
 
-
-
-
-        </ul>
-        <div className='flex items-center gap-6'>
-          <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
-
-        </div>
-        <div className='group relative'>
-          <img className='w-5 cursor-pointer'src={assets.prof  ile_icon} alt="" />
-
-        </div>
-
-        <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-           <p className='cursor-pointer hover:text-black'></p>
-            <p className='cursor-pointer  hover:text-black'></p>
-            <p className='cursor-pointer hover:text-black'></p>
-
-          </div>
-
-        </div>
-
+      <div className='flex items-center gap-6'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="Search Icon" />
         
-    )
+        <div className='group relative'> +
+          <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="Profile Icon" />
+          <div className='absolute right-0 mt-2 hidden group-hover:block bg-white shadow-md p-2 rounded'>
+            <p className='cursor-pointer hover:text-black'>My Profile</p>
+            <p className='cursor-pointer hover:text-black'>Orders</p>
+            <p className='cursor-pointer hover:text-black'>Logout</p>
+          </div>
+        </div>
+
+        <Link to='/cart' className='relative'>
+        <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
